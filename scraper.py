23549b592409b717ddbc1d7baaf626b139c1688e
alifmondolalif66-byte <alifmondolalif66@gmail.com>
2026-06-25 12:06:54 +0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 SERVER_URL = "https://luckyloop-position-update-16-05-e656.onrender.com"
-PHPSESSID  = os.environ.get("MW_PHPSESSID", "65e439roni6iu4rqmopb9skui6")
+PHPSESSID  = os.environ.get("MW_PHPSESSID", "hal2nphn031rjpk20j5gmtupg1")
 
 JOB_NAMES = [
     {"full": "TTV-Data Entry - PC required. Not for mobile phones. (E766-1470)", "short": "1470"},
